9a56b4afa2418a76420f35f05e313ce6ef1377cd
Blue <108904799+UnintelligibleGoofball@users.noreply.github.com>
2024-05-12 20:37:38 -0500

    got local image processing working!

--- Computer/localImageProcessing.py
+++ Computer/localImageProcessing.py
@@ -3,23 +3,31 @@
 #   python3 -m pip install --upgrade pip
 #   python3 -m pip install --upgrade Pillow
 
-
-import numpy as np
 from PIL import Image
-import matplotlib.colors
 
-img = Image.open('./HAT_ART.png')
-img = np.array(img).reshape(888)
-print(img)
-for i in range(8):
-    print((img[3*i], img[3*i+1], img[3*i+2]))
-imgArr = np.chararray(296, itemsize=6)
-for i in range(296):
-    print(i)
-    imgArr[i] = '%02x%02x%02x' % (img[3*i], img[3*i+1], img[3*i+2])
-    print(imgArr[i])
-imgArr.resize((8,37))
-print(imgArr)
-arr = np.save('./Pico/Art/HAT_ART_ARRAY', imgArr) # 37x8 array
-checkarr = np.load('./Pico/Art/HAT_ART_ARRAY.npy')
-print(checkarr)
+imgnam = input('what image file would you like to open?\n')
+
+#open image and make a list of tuples for each rgb value
+img = Image.open(imgnam,'r')
+img = list(img.getdata())
+
+#convert list of tuple to list of integers
+ImgList = []
+for i in img:
+    for j in i:
+        ImgList.append(j)
+
+filenam = input('What should the text list be called?\n')
+
+#create file and write list of integers as strings, each item on a new line
+imgFile = open(filenam,'w')
+for i in ImgList:
+    imgFile.write(str(i)+'\n')
+imgFile.close()
+
+#print file to confirm it was written correctly
+#imgFile = open(filenam,'r')
+#print(imgFile.read())
+
+#print info on the new file created
+print('New file '+filenam+' created, containing '+str(len(ImgList))+' items. Yahoo!')
--- Controller/code.py
+++ Controller/code.py
@@ -1,28 +1,16 @@
 # Hat originally made by Tony Baston of 3206
 # Re-wired and re-programmed by Blue S of 3206
 
-# SPDX-FileCopyrightText: 2018 Kattni Rembor for Adafruit Industries
-#
-# SPDX-License-Identifier: MIT
+#Testing a list of tuples in circuitpython
 
-"""CircuitPython Essentials NeoPixel example"""
-import time
-import board
-from rainbowio import colorwheel
-import neopixel
+list1=list()
+list1.append((255,0,0))
+list1.append((0,255,0))
+list1.append((0,0,255))
+print('list1 = ')
+print(list1)
 
-pixel_pin = board.A0
-num_pixels = 296
-
-pixels = neopixel.NeoPixel(pixel_pin, num_pixels, brightness=0.2, auto_write=False)
-
-def rainbow_cycle(wait):
-    for j in range(255):
-        for i in range(num_pixels):
-            rc_index = (i * 256 // num_pixels) + j
-            pixels[i] = colorwheel(rc_index & 255)
-        pixels.show()
-        time.sleep(wait)
-
-while True:
-    rainbow_cycle(0)
+list2=list()
+list2=((255,0,0),(0,255,0),(0,0,255))
+print('list2 = ')
+print(list2)
