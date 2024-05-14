3a33dc6eb35cd7919b4b6936c41c1429cd5a444e
Blue <108904799+UnintelligibleGoofball@users.noreply.github.com>
2024-05-14 18:21:24 -0500

    Worksgit add .

--- Controller/code.py
+++ Controller/code.py
@@ -3,6 +3,7 @@
 
 #open a file containing a list of colors and set each pixel to that color
 
+import time
 import board
 import neopixel
 
@@ -14,14 +15,20 @@ width = 37
 pixels = neopixel.NeoPixel(pixel_pin, (hight*width), brightness=brightness, auto_write=False)
 
 #reverse each width section but leave the height in the same order
-def flip_horizontal(list):
+def flip_horizontal(image):
     for i in range(hight+1):
-        list[width*(i-1):width*i] = list[width*(i-1):width*i][::-1]
+        image[width*(i-1):width*i] = image[width*(i-1):width*i][::-1]
 
 def disp_image(image):
     for i in range(hight*width):
         pixels[i] = image[i]
     pixels.show()
+
+def scroll_horizontal(image, position):
+    scrolled_image = list()
+    for i in range(hight+1):
+        scrolled_image[width*(i-1):width*i] = (image[position+(width*(i-1)):(width*i)] + image[width*(i-1):position+(width*(i-1))])
+    return(scrolled_image)
 
 file = open('HAT_ART_LIST.txt','r')
 pixelList = list()
@@ -36,5 +43,7 @@ pixelList2D = [ pixelList[i:i+3] for i in range(0, len(pixelList), 3) ]
 #flip the image horizontally, since tony wound the light strip clockwise and the images are read left to right
 flip_horizontal(pixelList2D)
 
-#display the image on the matrix
-disp_image(pixelList2D)
+while True:
+    for i in range(width):
+        time.sleep(.25)
+        disp_image(scroll_horizontal(pixelList2D, (width-i)))
